d09bf8d528d03c10b9094317bc84cfd26633e786
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2024-01-10 22:35:17 +0200

    task_3

---
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,14 @@
+import { RowID, RowElement } from './interface';
+
+export function insertRow(row: RowElement): number {
+  throw new Error('Function not implemented.');
+}
+
+export function updateRow(rowID: RowID, row: RowElement): RowID;
+{
+  throw new Error('Function not implemented.');
+}
+
+export function deleteRow(rowID: RowID): void {
+  throw new Error('Function not implemented.');
+}
