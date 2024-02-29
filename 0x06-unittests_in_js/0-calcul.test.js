01f2e06110692724a9fa50acc091d64e8baefc2a
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2024-02-29 21:56:35 +0200

    task 0

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -18,6 +18,22 @@ describe('calculateNumber', () => {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
 
+  it('should return -5', () => {
+    assert.strictEqual(calculateNumber(-1.5, -3.7), -5);
+  });
+
+  it('should return -2', () => {
+    assert.strictEqual(calculateNumber(1.4, -3.5), -2);
+  });
+
+  it('should return 3', () => {
+    assert.strictEqual(calculateNumber(-1.4, 3.5), 3);
+  });
+
+  it('should return -4', () => {
+    assert.strictEqual(calculateNumber(-1, -3), -4);
+  });
+
   it('shoild have type number', () => {
     assert.strictEqual(typeof calculateNumber(1, 3), 'number');
   });
