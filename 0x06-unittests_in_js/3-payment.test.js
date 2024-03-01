da4779102d1811a1c25a26fd7a67171764035caa
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2024-03-01 12:49:40 +0200

    task 3

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,11 +1,11 @@
-const spy = require('sinon').spy;
+const sinon = require('sinon');
 const sendPaymentRequestToApi = require('./3-payment');
 const Utils = require('./utils');
 const expect = require('chai').expect;
 
 describe('sendPaymentRequestToApi', () => {
   it('should Utils.calculateNumber called with ("SUM", 100, 20)', () => {
-    const spyUtlis = spy(Utils, 'calculateNumber');
+    const spyUtlis = sinon.spy(Utils, 'calculateNumber');
     sendPaymentRequestToApi(100, 20);
     expect(spyUtlis.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
     spyUtlis.restore();
